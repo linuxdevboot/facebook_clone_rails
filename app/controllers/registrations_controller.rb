22b4867aac0b2e37ed6d488a5fee27c12016440d
Daniel Schonfeld <danyschonfeld@gmail.com>
2016-03-18 15:06:25 -0700

    Added extra user attributes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :username)
+    params.require(:user).permit(:email, :password, :password_confirmation, :username, :age, :gender, :bio)
   end
 
   def account_update_params
