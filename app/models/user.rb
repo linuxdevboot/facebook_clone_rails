928e6f530db3ded66b0d4bb7746c06264622aa79
Daniel Schonfeld <danyschonfeld@gmail.com>
2016-03-18 16:49:38 -0700

    added friendships using self-referential associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :username
   validates_uniqueness_of :username
+
+  has_many :frienships, dependent: :destroy
+  has_many :inverse_frienships, class_name: "Frienship", foreign_key: "friend_id", dependent: :destroy
+
+  def create_friendship(user_2)
+    self.frienships.create(friend: user_2)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318215944) do
+ActiveRecord::Schema.define(version: 20160318233149) do
+
+  create_table "frienships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "state",       default: "pending"
+    t.datetime "friended_at"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
