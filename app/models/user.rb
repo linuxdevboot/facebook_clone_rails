51900050f5074b3314c8964818184abaaf873551
Daniel Schonfeld <danyschonfeld@gmail.com>
2016-03-18 17:20:01 -0700

    Added Friendship types

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,19 @@ class User < ActiveRecord::Base
   has_many :frienships, dependent: :destroy
   has_many :inverse_frienships, class_name: "Frienship", foreign_key: "friend_id", dependent: :destroy
 
-  def create_friendship(user_2)
+  def request_friendship(user_2)
     self.frienships.create(friend: user_2)
+  end
+
+  def active_friends
+    self.frienships.where(state: "active").map(&:friend) + self.inverse_frienships.where(state: "active").map(&:user)
+  end
+
+  def pending_friend_requests_to
+    self.frienships.where(state: "pending")
+  end
+
+  def pending_friend_requests_from
+    self.inverse_frienships.where(state: "pending")
   end
 end
