d0c1d2f7ccb6af9a29b39396e521807355ca092d
Daniel Schonfeld <danyschonfeld@gmail.com>
2016-03-20 02:59:24 -0700

    added styling to forms

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   end
 
   resources :posts
+
+  resources :activities, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
